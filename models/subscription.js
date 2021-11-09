700bdb9b759951d34aafac71364c49be040f6c1c
Alwin97 <2111Am1997>
2021-11-09 22:59:49 +0100

    - corrected types again, again

--- models/subscription.js
+++ models/subscription.js
@@ -4,23 +4,18 @@ const Schema = mongoose.Schema;
 const subscriptionSchema = new Schema({
   endpoint: {
     type: String,
-    required: true,
   },
   expirationTime: {
     type: String,
     default: null,
-    required: true
   },
   keys: {
     p256dh: {
       type: String,
-      required: true
     },
     auth: {
       type: String,
-      required: true
-    },
-    required: true
+    }
   },
 });
 
